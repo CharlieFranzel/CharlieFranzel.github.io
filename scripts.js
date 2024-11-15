63b4088bbb689f30855ba9aa23c5ac7d17ce5fc1
Charlie Franzel <charliefranzel@gmail.com>
2024-11-15 10:59:44 -0700

    done

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function resumeredirect() {
+    window.location.href = '/resume/index.html';
+}
+function contactredirect() {
+    window.location.href = '/contact/index.html';
+}
+function aboutmeredirect() {
+    window.location.href = '/index.html'
+}
